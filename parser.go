ff9562160a335db159054b6edc33033d7a4fd82f
critiqjo <john.ch.fr@gmail.com>
2016-01-14 02:58:46 +0530

    Parser: read contents

--- parser.go
+++ parser.go
@@ -2,7 +2,9 @@ package main
 
 import (
     "bufio"
+    "errors"
     "regexp"
+    "strconv"
 )
 
 type Request interface {
@@ -49,5 +51,45 @@ func ParseRequest(stream *bufio.Reader) Request {
         return &ReqRead { FileName: matches[1] }
     }
 
+    pat = regexp.MustCompile("^write ([^ \r\n]+) ([0-9]+) ?([0-9]+)?\r\n$")
+    matches = pat.FindStringSubmatch(str)
+    if len(matches) > 0 {
+        size, err := strconv.ParseUint(matches[2], 10, 64)
+        if err != nil { return nil }
+        var exp uint64
+        exp = 0
+        if len(matches[3]) > 0 {
+            exp, err = strconv.ParseUint(matches[3], 10, 64)
+            if err != nil { return nil }
+        }
+        contents, err := consumeWithCRLF(stream, size)
+        if err != nil { return nil }
+        return &ReqWrite { FileName: matches[1],
+                           Size: size,
+                           ExpTime: exp,
+                           Contents: contents,
+                         }
+    }
+
     return nil
+}
+
+func consumeWithCRLF(stream *bufio.Reader, size uint64) ([]byte, error) {
+    contents := make([]byte, size)
+    var readsize uint64
+    var n int
+    var e error
+    for readsize < size {
+        if n == 0 && e != nil {
+            return nil, e
+        }
+        slice := contents[readsize:]
+        n, e = stream.Read(slice)
+        readsize += uint64(n)
+    }
+    l, _ := stream.ReadSlice('\n')
+    if len(l) != 2 || l[0] != '\r' || l[1] != '\n' {
+        return nil, errors.New("Bad ending!")
+    }
+    return contents, nil
 }
