c8ddd658a4bd6f86fc4f6610c9d978142a1770df
critiqjo <john.ch.fr@gmail.com>
2016-01-20 02:02:06 +0530

    Add tests

---
+++ main_test.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "net"
+    "regexp"
+    "strconv"
+    "testing"
+    "time"
+)
+
+func init() {
+    go main()
+    time.Sleep(100 * time.Millisecond)
+}
+
+func newTest(t *testing.T, deadline time.Duration) (net.Conn, *bufio.Reader) {
+    conn, err := net.Dial("tcp", "localhost:8080")
+    if err != nil {
+        t.Fatal(err.Error())
+    }
+
+    err = conn.SetDeadline(time.Now().Add(deadline * time.Second))
+    if err != nil {
+        t.Fatal(err.Error())
+    }
+
+    return conn, bufio.NewReader(conn)
+}
+
+func TestBasics(t *testing.T) {
+    conn, rstream := newTest(t, 3)
+    defer conn.Close()
+
+    contents := "ab\r\ncd"
+
+    fmt.Fprintf(conn, "write abcd %v\r\n%v\r\n",
+                      len(contents), contents)
+    matches := expectLinePat(t, rstream, "OK ([0-9]+)\r\n")
+    ver1, _ := strconv.ParseUint(matches[1], 10, 64)
+
+    fmt.Fprintf(conn, "read abcd\r\n")
+    _ = expectLinePat(t, rstream, fmt.Sprintf("CONTENTS %v %v 0 ?\r\n",
+                                              ver1, len(contents)))
+    expectContents(t, rstream, []byte(contents))
+
+    contents = "qwer"
+
+    fmt.Fprintf(conn, "cas abcd %v %v\r\n%v\r\n",
+                      ver1, len(contents), contents)
+    matches = expectLinePat(t, rstream, "OK ([0-9]+)\r\n")
+    ver2, _ := strconv.ParseUint(matches[1], 10, 64)
+
+    if ver1 == ver2 {
+        t.Error("Version unchanged after successive writes")
+    }
+
+    fmt.Fprintf(conn, "read abcd\r\n")
+    _ = expectLinePat(t, rstream, fmt.Sprintf("CONTENTS %v %v 0 ?\r\n",
+                                              ver2, len(contents)))
+    expectContents(t, rstream, []byte(contents))
+
+    fmt.Fprintf(conn, "delete abcd\r\n")
+    _ = expectLinePat(t, rstream, "OK\r\n")
+
+    fmt.Fprintf(conn, "read abcd\r\n")
+    _ = expectLinePat(t, rstream, "ERR_FILE_NOT_FOUND\r\n")
+}
+
+func TestErrors(t *testing.T) {
+    conn, rstream := newTest(t, 3)
+    defer conn.Close()
+
+    fmt.Fprintf(conn, "write abcd\r\njunk\r\n")
+    _ = expectLinePat(t, rstream, "ERR_CMD_ERR\r\n")
+
+    conn, rstream = newTest(t, 3)
+    defer conn.Close()
+
+    contents := "qwer"
+    fmt.Fprintf(conn, "write abcd %v\r\n%v\r\n",
+                      len(contents), contents)
+    matches := expectLinePat(t, rstream, "OK ([0-9]+)\r\n")
+    ver, _ := strconv.ParseUint(matches[1], 10, 64)
+
+    fmt.Fprintf(conn, "cas abcd %v 4\r\njunk\r\n", ver + 1)
+    matches = expectLinePat(t, rstream, "ERR_VERSION\r\n")
+
+    fmt.Fprintf(conn, "read abcd\r\n")
+    _ = expectLinePat(t, rstream, fmt.Sprintf("CONTENTS %v %v 0 ?\r\n",
+                                              ver, len(contents)))
+    expectContents(t, rstream, []byte(contents))
+}
+
+func expectLinePat(t *testing.T, rstream *bufio.Reader, pattern string) []string {
+    str, err := rstream.ReadString('\n')
+    if err != nil {
+        t.Fatal(err.Error())
+    }
+    reg := regexp.MustCompile(pattern)
+    matches := reg.FindStringSubmatch(str)
+    if len(matches) == 0 {
+        t.Fatalf("Expected pattern %#v, found %#v", pattern, str)
+    }
+    return matches
+}
+
+func expectContents(t *testing.T, rstream *bufio.Reader, expected []byte) {
+    got := getContents(t, rstream, len(expected))
+    if len(expected) != len(got) {
+        t.Fatal("Contents mismatch")
+    } else {
+        for i, b := range expected {
+            if b != got[i] {
+                t.Fatal("Contents mismatch")
+            }
+        }
+    }
+}
+
+func getContents(t *testing.T, rstream *bufio.Reader, size int) []byte {
+    contents := make([]byte, size)
+    _, err := io.ReadFull(rstream, contents)
+    if err != nil {
+        t.Fatal("Bad contents:", err.Error())
+    }
+    trail, err := rstream.ReadSlice('\n')
+    if err != nil {
+        t.Fatal("Bad contents:", err.Error())
+    } else if len(trail) != 2 || trail[0] != '\r' || trail[1] != '\n' {
+        t.Fatal("Bad contents: did not end in CRLF")
+    }
+    return contents
+}
