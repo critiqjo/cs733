17a65f33199d049f92dbb1640b624900b7d7074e
critiqjo <john.ch.fr@gmail.com>
2016-02-18 00:32:04 +0530

    Tests, tests!

--- assignment2/consensus_test.go
+++ assignment2/consensus_test.go
@@ -11,7 +11,9 @@ type DummyMsger struct {
 func (self *DummyMsger) Register(notifch chan<- Message) {
     self.notifch = notifch
 }
-func (self *DummyMsger) Send(server int, msg Message) {}
+func (self *DummyMsger) Send(server int, msg Message) {
+    self.testch <- msg
+}
 func (self *DummyMsger) BroadcastVoteRequest(msg *VoteRequest) {
     self.testch <- msg
 }
@@ -24,9 +26,13 @@ func (pster *DummyPster) LogRead() []RaftEntry { return nil }
 func (pster *DummyPster) StateRead() *RaftState { return nil }
 func (pster *DummyPster) StateSave(ps *RaftState) {}
 
-type DummyMachn struct {}
-func (machn *DummyMachn) ApplyLazy(reqs []ClientEntry) {}
-func (machn *DummyMachn) RespondIfSeen(uid uint64) bool { return false }
+type DummyMachn struct {
+    msger *DummyMsger
+}
+func (self *DummyMachn) ApplyLazy(reqs []ClientEntry) {
+    self.msger.testch <- reqs
+}
+func (self *DummyMachn) RespondIfSeen(uid uint64) bool { return false }
 
 func TestDummy(t *testing.T) {
     assert := func(e bool, args ...interface{}) {
@@ -35,12 +41,41 @@ func TestDummy(t *testing.T) {
         }
     }
 
-    msger, pster, machn := &DummyMsger{ nil, make(chan interface{}) }, &DummyPster{}, &DummyMachn{}
+    msger := &DummyMsger{ nil, make(chan interface{}) }
+    pster, machn := &DummyPster{}, &DummyMachn{ msger }
     raft := NewRaftNode(0, msger, pster, machn)
     go raft.Run(func() time.Duration {
         return time.Duration(rand.Int() % 400 + 200) * time.Millisecond
     })
 
-    m := <-msger.testch // wait for timeout
-    assert(*m.(*VoteRequest) == VoteRequest { 1, 0, 0, 0 })
+    var m interface{}
+    msger.notifch <- &AppendEntries {
+        Term: 1,
+        LeaderId: 2,
+        PrevLogIdx: 0,
+        PrevLogTerm: 0,
+        Entries: []ClientEntry { ClientEntry { 23425, nil } },
+        CommitIdx: 0,
+    }
+    m = <-msger.testch
+    assert(*m.(*AppendReply) == AppendReply { 1, true }, "bad append", m)
+
+    m = <-msger.testch // wait for timeout
+    assert(*m.(*VoteRequest) == VoteRequest { 2, 0, 1, 1 }, "bad votereq", m)
+
+    m = <-msger.testch // wait for timeout again
+    assert(*m.(*VoteRequest) == VoteRequest { 3, 0, 1, 1 }, "bad votereq", m)
+
+    msger.notifch <- &AppendEntries {
+        Term: 3,
+        LeaderId: 3,
+        PrevLogIdx: 1,
+        PrevLogTerm: 1,
+        Entries: nil,
+        CommitIdx: 1,
+    }
+    m = <-msger.testch
+    assert(*m.(*AppendReply) == AppendReply { 3, true }, "bad append", m)
+    m = <-msger.testch // apply lazy
+    assert(m.([]ClientEntry)[0] == ClientEntry { 23425, nil }, "bad apply", m)
 }
